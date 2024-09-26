b23b9ef4094243cae83c6dae560228706f253b70
lavishav <lavishaverma0905@gmail.com>
2024-09-26 11:27:16 +0530

    all files added

---
+++ ok.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float a, b, c;
+    printf("enter value of a:");
+    scanf("%f", &a);
+    printf("enter value of b:");
+    scanf("%f", &b);
+    c = a + b;
+    printf("addition:", c);
+    return 0;
+}
